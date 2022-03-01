da32cbc54e0089d32b2f1aeadc8a41b9180271cf
Roarnotes <Roarnotesfoundation@gmail.com>
2022-02-28 22:14:55 -1100

    slider btn border follower

--- Js/app.js
+++ Js/app.js
@@ -1,5 +1,26 @@
+// events
+// nav discover-btn
+const $candiscover = document.getElementById("discover-btn");
+$candiscover.addEventListener("click", $discover);
+
+function $discover(params) {
+  document.getElementById("trending").style.display = "inline";
+
+  document.getElementById("trending-btn").style.borderBottom = "solid #0E0E0E";
+}
+
+//dropdown discover-btn
+const $candiscover11 = document.getElementById("discover-btn1");
+$candiscover11.addEventListener("click", $discover11);
+
+function $discover11(params) {
+  document.getElementById("trending").style.display = "inline";
+
+  document.getElementById("trending-btn").style.borderBottom = "solid #0E0E0E";
+}
+//slider controller
+
 // treanding 
-// slider controller 
 const $cantrending = document.getElementById("trending-btn");
 $cantrending.addEventListener("click", trending);
 
@@ -8,6 +29,11 @@ function trending(params) {
   document.getElementById("localized").style.display = "none";
   document.getElementById("mover1").style.display = "none"; 
   document.getElementById("you").style.display = "none";
+
+  document.getElementById("trending-btn").style.borderBottom = "solid #0E0E0E";
+  document.getElementById("localized-btn").style.borderBottom = "none";
+  document.getElementById("online-btn").style.borderBottom = "none";
+  document.getElementById("you-btn").style.borderBottom = "none";
 }
 
 // localized-btn
@@ -18,7 +44,12 @@ function localized(params) {
   document.getElementById("trending").style.display = "none";
   document.getElementById("localized").style.display = "inline";
   document.getElementById("mover1").style.display = "none";
-   document.getElementById("you").style.display = "none";
+  document.getElementById("you").style.display = "none";
+
+  document.getElementById("trending-btn").style.borderBottom = "none";
+  document.getElementById("localized-btn").style.borderBottom = "solid #0E0E0E";
+  document.getElementById("online-btn").style.borderBottom = "none";
+  document.getElementById("you-btn").style.borderBottom = "none";
 }
 
 //Online-btn
@@ -30,6 +61,11 @@ function cmover1(params) {
   document.getElementById("localized").style.display = "none";
   document.getElementById("mover1").style.display = "inline"; 
   document.getElementById("you").style.display = "none";
+
+  document.getElementById("trending-btn").style.borderBottom = "none";
+  document.getElementById("localized-btn").style.borderBottom = "none";
+  document.getElementById("online-btn").style.borderBottom = "solid #0E0E0E";
+  document.getElementById("you-btn").style.borderBottom = "none";
 }
 
 //you-btn
@@ -41,4 +77,9 @@ function cancel1(params) {
   document.getElementById("localized").style.display = "none";
   document.getElementById("you").style.display = "inline";
   document.getElementById("mover1").style.display = "none";
+
+  document.getElementById("trending-btn").style.borderBottom = "none";
+  document.getElementById("localized-btn").style.borderBottom = "none";
+  document.getElementById("online-btn").style.borderBottom = "none";
+  document.getElementById("you-btn").style.borderBottom = "solid #0E0E0E";
 }
